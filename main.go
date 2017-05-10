fa0a17664ca2ef286d56373cf2aa8a3ef4f952a1
renyangwei <18673298768@163.com>
2017-05-10 23:43:29 +0800

    add 3 files and add group

--- main.go
+++ main.go
@@ -16,74 +16,97 @@ import (
 
 const (
 	//主机地址
-	HTTP_HOST string = "http://180.76.163.58:8081/factory"
-	//	HTTP_HOST        string = "http://localhost:8081/factory"
+	//	HTTP_HOST string = "http://180.76.163.58:8081/factory"
+	HTTP_HOST        string = "http://localhost:8081/factory"
 	HTTP_APPLICATION string = "application/json;charset=utf-8"
 	FILE_NAME        string = "infor.txt"
+	GROUP_NAME       string = "一号线"
+	FILE_NAME_1      string = "infor1.txt"
+	GROUP_NAME_1     string = "二号线"
+	FILE_NAME_2      string = "infor2.txt"
+	GROUP_NAME_2     string = "三号线"
 )
+
+type RealTimeDataJson struct {
+	Factory string `json:"Factory"`
+	Other   string `json:"Other"`
+	Group   string `json:"Group"`
+}
 
 func main() {
 	c := cron.New()
 	//秒 分 时 日 月 星期
 	//	spec := "0 */1 * * * *" //每分钟一次
-	spec := "*/5 * * * * *"
+	spec := "*/5 * * * * *" //每五秒一次
 	c.AddFunc(spec, func() {
-		//读取文件
-		fd, err := os.Open(FILE_NAME)
-		if err != nil {
-			log.Println("open file err:", err.Error())
-		}
-		br := bufio.NewReader(fd)
-		r, _, err := br.ReadRune()
-		if err != nil {
-			log.Println("read rune err:", err.Error())
-		}
-		if r != '\uFEFF' {
-			br.UnreadRune() // Not a BOM -- put the rune back
-		}
-		fileContent := ""
-		for {
-			str, err := br.ReadString('\n')
-			fileContent = fileContent + str
-			if err == io.EOF {
-				break
-			}
-		}
-		log.Println("fileContent:", fileContent)
-
-		if fd.Close() != nil {
-			log.Println("file.close err:", err.Error())
-		}
-
-		//判断格式
-		if !checkData(fileContent) {
-			log.Println("check data failed")
-			return
-		}
-
-		//解析完成后通过http协议发送到服务端
-		httpPost(fileContent)
-
+		readFile(FILE_NAME, GROUP_NAME)
+		readFile(FILE_NAME_1, GROUP_NAME_1)
+		readFile(FILE_NAME_2, GROUP_NAME_2)
 	})
 	c.Start()
 	select {} //阻塞主线程不退出
 }
 
 /*
-判断是否为json格式
+读取文件
+fileName	文件名
+group		班组
 */
-func checkData(data string) bool {
-	type CheckJson struct {
-		Factory string `json:"Factory"`
-		Other   string `json:"Other"`
+func readFile(fileName string, group string) {
+	//读取文件
+	fd, err := os.Open(fileName)
+	if err != nil {
+		log.Println("open file err:", err.Error())
+		return
 	}
-	var checkJson CheckJson
-	err := json.Unmarshal([]byte(data), &checkJson)
+	br := bufio.NewReader(fd)
+	r, _, err := br.ReadRune()
+	if err != nil {
+		log.Println("read rune err:", err.Error())
+		return
+	}
+	if r != '\uFEFF' {
+		br.UnreadRune() // Not a BOM -- put the rune back
+	}
+	fileContent := ""
+	for {
+		str, err := br.ReadString('\n')
+		fileContent = fileContent + str
+		if err == io.EOF {
+			break
+		}
+	}
+	log.Println("fileContent:", fileContent)
+
+	if fd.Close() != nil {
+		log.Println("file.close err:", err.Error())
+		return
+	}
+
+	//增加产线
+	fileContent = addGroup(fileContent, group)
+
+	//解析完成后通过http协议发送到服务端
+	httpPost(fileContent)
+}
+
+/*
+添加产线
+*/
+func addGroup(data string, group string) string {
+	var fileContentJson RealTimeDataJson
+	err := json.Unmarshal([]byte(data), &fileContentJson)
 	if err != nil {
 		log.Println("json data invalid")
-		return false
+		return err.Error()
 	}
-	return true
+	fileContentJson.Group = group
+	fileContent, err := json.Marshal(fileContentJson)
+	if err != nil {
+		log.Println("json to string failed")
+		return err.Error()
+	}
+	return string(fileContent)
 }
 
 /*
@@ -99,7 +122,7 @@ func httpPost(data string) {
 	defer resp.Body.Close()
 	respBody, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		// handle error
+		log.Println(err)
 	}
 
 	log.Println(string(respBody))
