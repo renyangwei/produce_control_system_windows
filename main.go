5bc10ba5bf611061ee89438b83b6c56f2fa78e55
renyangwei <18673298768@163.com>
2017-07-12 22:54:46 +0800

    do not show log

--- main.go
+++ main.go
@@ -7,9 +7,10 @@ import (
 	"encoding/json"
 	"io"
 	"io/ioutil"
-	"log"
+	//"log"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/robfig/cron"
 )
@@ -18,34 +19,17 @@ const (
 	//主机地址
 	HTTP_URL_FACTORY string = "http://180.76.163.58:8081/factory"
 	HTTP_URL_HISTORY string = "http://180.76.163.58:8081/history"
+	HTTP_URL_FORCE   string = "http://180.76.163.58:8081/force"
 	//	HTTP_URL_FACTORY string = "http://localhost:8081/factory"
 	//	HTTP_URL_HISTORY string = "http://localhost:8081/history"
+	//	HTTP_URL_FORCE   string = "http://localhost:8081/force"
+
 	HTTP_APPLICATION string = "application/json;charset=utf-8"
-	FILE_NAME        string = "infor.txt"
-	GROUP_NAME       string = "一号线"
-	FILE_NAME_1      string = "infor1.txt"
-	GROUP_NAME_1     string = "二号线"
-	FILE_NAME_2      string = "infor2.txt"
-	GROUP_NAME_2     string = "三号线"
-	FILE_NAME_DATA   string = "data.txt"
-	FILE_NAME_DATA_1 string = "data1.txt"
-	FILE_NAME_DATA_2 string = "data2.txt"
+
+	FILE_PATH string = "path.txt"
 )
 
-type RealTimeDataJson struct {
-	Factory string `json:"Factory"`
-	Other   string `json:"Other"`
-	Group   string `json:"Group"`
-}
-
-type HistoryDataJson struct {
-	Factory string `json:"Factory"`
-	Other   string `json:"Other"`
-	Time    string `json:"Time"`
-	Class   string `json:"Class"`
-	Group   string `json:"Group"`
-}
-
+//文件内容
 type DataJson struct {
 	Factory string `json:"Factory"`
 	Other   string `json:"Other"`
@@ -54,20 +38,59 @@ type DataJson struct {
 	Group   string `json:"Group"`
 }
 
+//返回数据
+type ResponseJson struct {
+	Factory string `json:"Factory"`
+	Group   string `json:"Group"`
+	Class   string `json:"Class"`
+	Time    string `json:"Time"`
+}
+
+var factoryName string
+
 func main() {
 	c := cron.New()
 	//秒 分 时 日 月 星期
 	//	spec := "0 */1 * * * *" //每分钟一次
 	spec := "*/5 * * * * *" //每五秒一次
 	c.AddFunc(spec, func() {
-		readFile(FILE_NAME, GROUP_NAME, HTTP_URL_FACTORY)
-		readFile(FILE_NAME_1, GROUP_NAME_1, HTTP_URL_FACTORY)
-		readFile(FILE_NAME_2, GROUP_NAME_2, HTTP_URL_FACTORY)
+		FILE_NAME_ARRAY := []string{"infor.txt", "infor1.txt", "infor2.txt", "infor3.txt", "infor4.txt", "infor5.txt"}
+		FILE_NAME_DATA_ARRAY := []string{"data.txt", "data1.txt", "data2.txt", "data3.txt", "data4.txt", "data5.txt"}
+		GROUP_NAME_ARRAY := []string{"一号线", "二号线", "三号线", "四号线", "五号线", "六号线"}
 
-		readFile(FILE_NAME_DATA, GROUP_NAME, HTTP_URL_HISTORY)
-		readFile(FILE_NAME_DATA_1, GROUP_NAME_1, HTTP_URL_HISTORY)
-		readFile(FILE_NAME_DATA_2, GROUP_NAME_2, HTTP_URL_HISTORY)
-
+		//读取文件路径
+		path, err := os.Open(FILE_PATH)
+		if err != nil {
+			//			log.Println("open file path error,", err.Error())
+			return
+		}
+		reader := bufio.NewReader(path)
+		r, _, err := reader.ReadRune()
+		if err != nil {
+			//			log.Println("read rune err:", err.Error())
+			return
+		}
+		if r != '\uFEFF' {
+			reader.UnreadRune() // Not a BOM -- put the rune back
+		}
+		pathContent, err := reader.ReadString('\n')
+		if err != nil {
+			//			log.Println("read string path error,", err.Error())
+			return
+		}
+		//		log.Println("pathContent:", pathContent)
+		pathArray := strings.Split(pathContent, ",")
+		for i := 0; i < len(pathArray); i++ {
+			pathArr := pathArray[0]
+			if i == len(pathArray)-1 {
+				pathArr = pathArray[i][0 : len(pathArray[i])-2]
+			}
+			//			log.Println("pathArr,", pathArr)
+			fileName := pathArr + FILE_NAME_ARRAY[i]
+			dataName := pathArr + FILE_NAME_DATA_ARRAY[i]
+			readFile(fileName, GROUP_NAME_ARRAY[i], HTTP_URL_FACTORY)
+			readFile(dataName, GROUP_NAME_ARRAY[i], HTTP_URL_HISTORY)
+		}
 	})
 	c.Start()
 	select {} //阻塞主线程不退出
@@ -82,13 +105,13 @@ func readFile(fileName string, group string, httpUrl string) {
 	//读取文件
 	fd, err := os.Open(fileName)
 	if err != nil {
-		log.Println("open file err:", err.Error())
+		//		log.Println("open file err:", err.Error())
 		return
 	}
 	br := bufio.NewReader(fd)
 	r, _, err := br.ReadRune()
 	if err != nil {
-		log.Println("read rune err:", err.Error())
+		//		log.Println("read rune err:", err.Error())
 		return
 	}
 	if r != '\uFEFF' {
@@ -102,16 +125,15 @@ func readFile(fileName string, group string, httpUrl string) {
 			break
 		}
 	}
-	log.Println("fileContent:", fileContent)
+	//	log.Println("fileContent:", fileContent)
 
 	if fd.Close() != nil {
-		log.Println("file.close err:", err.Error())
+		//		log.Println("file.close err:", err.Error())
 		return
 	}
-
 	//增加产线
 	fileContent = addGroup(fileContent, group)
-	log.Println("after add group fileContent:", fileContent)
+	//	log.Println("after add group fileContent:", fileContent)
 	//解析完成后通过http协议发送到服务端
 	httpPost(fileContent, httpUrl)
 }
@@ -123,15 +145,17 @@ func addGroup(data string, group string) string {
 	var fileContentJson DataJson
 	err := json.Unmarshal([]byte(data), &fileContentJson)
 	if err != nil {
-		log.Println("json data invalid")
+		//		log.Println("json data invalid")
 		return err.Error()
 	}
 	fileContentJson.Group = group
 	fileContent, err := json.Marshal(fileContentJson)
 	if err != nil {
-		log.Println("json to string failed")
+		//		log.Println("json to string failed")
 		return err.Error()
 	}
+	//保存
+	factoryName = fileContentJson.Factory
 	return string(fileContent)
 }
 
@@ -142,15 +166,57 @@ func httpPost(data string, httpUrl string) {
 	body := bytes.NewBuffer([]byte(data))
 	resp, err := http.Post(httpUrl, HTTP_APPLICATION, body)
 	if err != nil {
-		log.Println(err)
+		//		log.Println(err)
 	}
-
 	defer resp.Body.Close()
 	respBody, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		log.Println(err)
+		//		log.Println(err)
 	}
 
-	log.Println(string(respBody))
+	//	log.Println(string(respBody))
+	parseIsForceRefresh(respBody)
+}
 
+/*
+增加判断返回内容
+*/
+func parseIsForceRefresh(response []byte) {
+	var responseJson ResponseJson
+	err := json.Unmarshal(response, &responseJson)
+	if err != nil {
+		//		log.Println(err.Error())
+		return
+	}
+	var fileContent string = "{\"Class\":\"" + responseJson.Class + "\", \"Time\": \"" + responseJson.Time + "\"}"
+	var fileName string
+	//判断文件名
+	switch responseJson.Group {
+	case "一号线":
+		fileName = "location.txt"
+	case "二号线":
+		fileName = "location1.txt"
+	case "三号线":
+		fileName = "location2.txt"
+	case "四号线":
+		fileName = "location3.txt"
+	case "五号线":
+		fileName = "location4.txt"
+	case "六号线":
+		fileName = "location5.txt"
+	}
+	writeFile(fileName, fileContent)
+}
+
+/*
+写入文件
+*/
+func writeFile(fileName string, fileContent string) {
+	//	log.Println("fileName=", fileName, ",fileContent=", fileContent)
+	var d1 = []byte(fileContent)
+	err := ioutil.WriteFile(fileName, d1, 0666) //写入文件(字节数组)
+	if err != nil {
+		//		log.Println(err.Error())
+		return
+	}
 }
--- readme.md
+++ readme.md
@@ -10,5 +10,16 @@
 
 ### infor文件 ###
 
-修改**infor.txt**文件中**Factory**和**Other**对应的数据（数据格式不能变），保存，看到**PaperManagementClient.exe**的窗口中提示**post success**后，等待APP自动刷新，就能看到修改后的数据
+**infor.txt**文件中的数据表示实时数据，修改**infor.txt**文件中**Factory**和**Other**对应的数据（数据格式不能变），保存，看到**PaperManagementClient.exe**的窗口中提示**post success**后，等待APP自动刷新，就能看到修改后的数据，**infor.txt** 代表 **一号线**， **infor1.txt** 代表**二号线**，以此类推
 
+### data文件 ###
+
+**data.txt**文件中的数据表示历史数据，修改**data.txt**中的数据，保存，看到 **PaperManagementClient.exe**的窗口中提示**post success**后，进入APP中**历史数据**界面，可以查询对应时间、产线和班组的数据。**data.txt** 代表 **一号线**， **data1.txt** 代表 **二号线**， 以此类推
+
+### 手动刷新功能 ###
+
+在APP的**历史数据**界面中，长按**查询**，弹出对话框提示 **系统将重新读取数据，耗时较长**，点击确认即可手动刷新，如果在5秒内刷新失败，用户可以稍后再次查询。用户确定手动刷新后PaperManagementClient.exe程序目录下会生成location.txt文件，生管系统读取文件内容，将数据写入对应的**data.txt** 中
+
+### path文件 ###
+
+PaperManagementClient.exe程序读取**data.txt**和**infor.txt**的路径，每个路径之间用逗号隔开，最后一个路径后加上回车符号
